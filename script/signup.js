4e1e4330ed3b6ab44ab1c7b8a789b1e1b57c1eb3
abla-code <abla24rh@student.ju.se>
2025-05-06 18:44:23 +0200

    testing the responsivness of the register button. Commented out the font link since it was messing up with the console log.

--- script/signup.js
+++ script/signup.js
@@ -1,21 +1,29 @@
-const form = document.getElementById('register-form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const user = {
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('register-form');
+    if (!form) {
+    //   console.error("Form not found");
+      return;
+    }
+  
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const user = {
         name: form.name.value,
         email: form.email.value,
         password: form.password.value
-    };
-
-    const users = JSON.parse (localStorage.getItem('users')) || [];
-
-    if (users.find(u => u.email === user.email)){
-        alert('user already exist');
+      };
+  
+      const users = JSON.parse(localStorage.getItem('users')) || [];
+  
+      if (users.find(u => u.email === user.email)) {
+        alert('User already exists');
         return;
-    }
-
-    users.push(user);
-    localStorage.setItem('users',JSON.stringify(users));
-
-   window.location.href= '/profile';
-});
+      }
+  
+      users.push(user);
+      localStorage.setItem('users', JSON.stringify(users));
+  
+      window.location.href = '../profile.html';
+    });
+  });
+  
