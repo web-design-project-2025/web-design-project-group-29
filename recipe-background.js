b48b15a1048b64d02b789876228c75ce6a1d61bc
afuahdodeshi <almo23mt@student.ju.se>
2025-05-16 09:23:33 +0200

    added the remaing error catching to the javascript files using try and catch in most of them

--- recipe-background.js
+++ recipe-background.js
@@ -1,5 +1,6 @@
 // help for the data loading from chatgpt https://chatgpt.com/share/680c970d-03cc-8005-ac67-04fadbeec9e0
 document.addEventListener ("DOMContentLoaded", () => {
+    try{
     fetch('recipe-backdrop.json')
     .then (response => response.json())
     .then(recipesBckg =>{
@@ -15,6 +16,9 @@ document.addEventListener ("DOMContentLoaded", () => {
         } else {
             container.innerHTML = "<p>No image found for this country<p>";
         }
-    })
-    .catch (error => console.error("Error fetching image:", error))
-})
+    });
+} catch(error){
+    console.error("Error fetching image:",error);
+}
+
+});
